15f3a57b5dd0b4f871a89c9beabc3e8ee2a882bb
xiluan <xinyujie0320@gmail.com>
2023-11-27 19:24:19 -0800

    #186557511: Undertow Example (#118)
    * Refactored jakarta servlet examples into tomcat folder.
    
    * #186557511 : Added example for jakarta servlet application using undertow instead of tomcat.

---
+++ examples-jakarta/spring-boot-starter-example-undertow/src/main/java/com/moesif/moesifservlet/example/undertow/MyConfig.java
@@ -0,0 +1,65 @@
+package com.moesif.moesifservlet.example.undertow;
+
+import jakarta.servlet.Filter;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+
+import com.moesif.servlet.MoesifConfiguration;
+import org.springframework.web.servlet.config.annotation.*;
+import org.springframework.context.annotation.*;
+import com.moesif.servlet.MoesifFilter;
+import org.springframework.web.servlet.view.InternalResourceViewResolver;
+
+
+@Configuration
+@EnableWebMvc
+@ComponentScan
+public class MyConfig implements WebMvcConfigurer {
+
+    public String applicationId = "Your Moesif Application Id";
+    @Bean
+    public Filter moesifFilter() {
+
+        MoesifConfiguration config = new MoesifConfiguration() {
+
+            @Override
+            public String identifyUser(HttpServletRequest request, HttpServletResponse response) {
+                return request.getHeader("X-User-Id");
+//                if (request.getUserPrincipal() == null) {
+//                    return null;
+//                }
+//                return request.getUserPrincipal().getName();
+            }
+
+            @Override
+            public String getSessionToken(HttpServletRequest request, HttpServletResponse response) {
+                return request.getHeader("Authorization");
+            }
+
+            @Override
+            public String identifyCompany(HttpServletRequest request, HttpServletResponse response) {
+                return request.getHeader("X-Company-Id");
+            }
+
+            @Override
+            public String getApiVersion(HttpServletRequest request, HttpServletResponse response) {
+                return request.getHeader("X-Api-Version");
+            }
+        };
+
+        MoesifFilter moesifFilter = new MoesifFilter(applicationId, config, true);
+
+        // Set flag to log request and response body
+        moesifFilter.setLogBody(true);
+
+        return moesifFilter;
+    }
+
+    @Bean
+    public InternalResourceViewResolver viewResolver() {
+        InternalResourceViewResolver resolver = new InternalResourceViewResolver();
+        // resolver.setPrefix("/WEB-INF/jsp/");
+        // resolver.setSuffix(".jsp");
+        return resolver;
+    }
+}

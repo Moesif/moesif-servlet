e13ca31b281b281d4f9b049f24d2909978ba4efd
Brian Kennedy <brian@moesif.com>
2024-11-15 19:29:20 -0800

    Format

--- moesif-servlet/src/main/java/com/moesif/servlet/wrappers/BodyHandler.java
+++ moesif-servlet/src/main/java/com/moesif/servlet/wrappers/BodyHandler.java
@@ -1,9 +1,5 @@
 package com.moesif.servlet.wrappers;
-import org.apache.commons.io.output.ByteArrayOutputStream;
-import org.apache.commons.lang3.StringUtils;
 
-import java.io.IOException;
-import java.io.InputStream;
 import java.io.UnsupportedEncodingException;
 import java.nio.charset.StandardCharsets;
 import java.util.HashMap;
@@ -11,22 +7,22 @@ import java.util.Map;
 
 public class BodyHandler {
 
-    public static boolean logBody = true;
-    public static final int MAX_BODY_SIZE = 10; // Move to a common utility class
+  public static final int MAX_BODY_SIZE = 10; // Move to a common utility class
+  public static boolean logBody = true;
 
-    public static String encodeContent(byte[] content, String encoding)  {
-        try {
-        return new String(content, encoding != null ? encoding : StandardCharsets.UTF_8.name());
-        } catch (UnsupportedEncodingException e) {
-          return "[UNSUPPORTED ENCODING]";
-        }
+  public static String encodeContent(byte[] content, String encoding) {
+    try {
+      return new String(content, encoding != null ? encoding : StandardCharsets.UTF_8.name());
+    } catch (UnsupportedEncodingException e) {
+      return "[UNSUPPORTED ENCODING]";
     }
+  }
 
-    // a method that returns a simple java map representing an error message for large body meant to be serialized into json
-    public static Map<String, String> getLargeBodyError(long contentLength) {
-        Map<String, String> error = new HashMap<>();
-        error.put("msg", "request.body.length " + contentLength + " exceeded requestMaxBodySize of  " + MAX_BODY_SIZE + " bytes");
-        return error;
-    }
+  // a method that returns a simple java map representing an error message for large body meant to be serialized into json
+  public static Map<String, String> getLargeBodyError(long contentLength) {
+    Map<String, String> error = new HashMap<>();
+    error.put("msg", "request.body.length " + contentLength + " exceeded requestMaxBodySize of  " + MAX_BODY_SIZE + " bytes");
+    return error;
+  }
 
 }
